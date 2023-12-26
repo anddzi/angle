d1547d65c1a94106b5ffb70dbcdf1ba4bd085375
Andrei Dzilanian <andrei.dzilanian@gmail.com>
2023-12-26 13:14:33 +0100

    added router&lazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  {
+    path: 'content',
+    loadChildren: () =>
+      import('./modules/content/content.module').then(m => m.ContentModule),
+  },
+];
--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Item {title: string; url: string;}
 
 @Component({
   selector: 'app-example',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './example.component.html',
   styleUrl: './example.component.scss'
 })
-interface Item {title: string; url: string;}
 
 export class ExampleComponent {
   items: Item[] = [
